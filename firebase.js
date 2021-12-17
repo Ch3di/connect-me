20862fd90f3777ddbaada77407376077ed19a3b5
Chady Bouhlel <chady@example.com>
2021-12-17 12:40:52 +0100

    updated the firebase file

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "connect-me-8d27b.firebaseapp.com",
-    projectId: "connect-me-8d27b",
-    storageBucket: "connect-me-8d27b.appspot.com",
-    messagingSenderId: "486511477203",
-    appId: "1:486511477203:web:dcc91ce684a107bff02f26",
+  apiKey: "",
+  authDomain: "connect-me-8d27b.firebaseapp.com",
+  projectId: "connect-me-8d27b",
+  storageBucket: "connect-me-8d27b.appspot.com",
+  messagingSenderId: "486511477203",
+  appId: "1:486511477203:web:dcc91ce684a107bff02f26"
 };
 
 const app = initializeApp(firebaseConfig);
