e82fe04c0eca023dad4dbb13c2dd33fdddcf2811
Wael <youssfi.wael1997@gmail.com>
2021-12-17 10:35:16 +0100

    added HomeScreen

--- App.js
+++ App.js
@@ -4,31 +4,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 const globalScreenOptions = {
-  headerStyle: { backgroundColor: "#2C6BED" },
-  headerTitleStyle: { color: "white" },
-  headerTintColor: "white"
+    headerStyle: { backgroundColor: "#2C6BED" },
+    headerTitleStyle: { color: "white" },
+    headerTintColor: "white",
 };
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={globalScreenOptions}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={globalScreenOptions}>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,86 +1,94 @@
 import { StatusBar } from "expo-status-bar";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState, useLayoutEffect } from "react";
 import { View, StyleSheet, KeyboardAvoidingView } from "react-native";
 import { Button, Input, Text } from "react-native-elements";
 import { auth } from "../firebase";
 
 function RegisterScreen({ navigation }) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerBackTitle: "Login"
-    });
-  }, [navigation]);
-
-  const register = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((authUser) => {
-        authUser.user.update({
-          displayName: name,
-          photoURL: imageUrl || ""
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerBackTitle: "Login",
         });
-      })
-      .catch((error) => alert(error.message));
-  };
+    }, [navigation]);
 
-  return (
-    <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      <StatusBar style="light" />
-      <Text h3 style={{ marginBottom: 50, textAlign: "center" }}>
-        Create a Connect-ME account
-      </Text>
-      <View style={styles.inputContainer}>
-        <Input
-          placeholder="Full name"
-          autoFocus
-          value={name}
-          onChange={(text) => setName(text)}
-        />
-        <Input
-          placeholder="Email"
-          value={email}
-          onChange={(text) => setEmail(text)}
-        />
-        <Input
-          placeholder="Password"
-          secureTextEntry
-          value={password}
-          onChange={(text) => setPassword(text)}
-        />
-        <Input
-          placeholder="Default Picture Url"
-          value={imageUrl}
-          onChange={(text) => setImageUrl(text)}
-          onSubmitEditing={register}
-        />
-      </View>
-      <View style={styles.button}>
-        <Button raised title="Register" onPress={register} />
-      </View>
-    </KeyboardAvoidingView>
-  );
+    const register = () => {
+        createUserWithEmailAndPassword(auth, email, password).catch((error) =>
+            alert(error.code)
+        );
+        // .then((userCredential) => {
+        //     const user = userCredential.user;
+        //     alert(user);
+        // })
+        // .catch((error) => alert(error.code));
+        // auth.createUserWithEmailAndPassword(email, password)
+        //     .then((authUser) => {
+        //         authUser.user.update({
+        //             displayName: name,
+        //             photoURL: imageUrl || "",
+        //         });
+        //     })
+        //     .catch((error) => alert(error.message));
+    };
+
+    return (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
+            <Text h3 style={{ marginBottom: 50, textAlign: "center" }}>
+                Create a Connect-ME account
+            </Text>
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder="Full name"
+                    autoFocus
+                    value={name}
+                    onChange={(text) => setName(text)}
+                />
+                <Input
+                    placeholder="Email"
+                    value={email}
+                    onChange={(text) => setEmail(text)}
+                />
+                <Input
+                    placeholder="Password"
+                    secureTextEntry
+                    value={password}
+                    onChange={(text) => setPassword(text)}
+                />
+                <Input
+                    placeholder="Default Picture Url"
+                    value={imageUrl}
+                    onChange={(text) => setImageUrl(text)}
+                    onSubmitEditing={register}
+                />
+            </View>
+            <View style={styles.button}>
+                <Button raised title="Register" onPress={register} />
+            </View>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-    backgroundColor: "white"
-  },
-  inputContainer: {
-    width: 300
-  },
-  button: {
-    marginTop: 20,
-    width: 300
-  }
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
+    inputContainer: {
+        width: 300,
+    },
+    button: {
+        marginTop: 20,
+        width: 300,
+    },
 });
 
 export default RegisterScreen;
