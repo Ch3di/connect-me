6b54bd0321578297374c594305433a3cc33c1efb
Chady Bouhlel <chady@example.com>
2021-12-17 12:49:29 +0100

    implemented HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-elements";
+import CustomListItem from "../components/CutomListItem";
+import { auth } from "../firebase";
 
 const HomeScreen = () => {
-    return (
-        <View>
-            <Text>Home page</Text>
-        </View>
-    );
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Connect-ME",
+      headerStyle: {
+        backgroundColor: "#fff",
+        headerTitleStyle: {
+          color: "black"
+        },
+        headerTintColor: "black",
+        headerLeft: () => (
+          <View style={{ marginLeft: 20 }}>
+            <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+          </View>
+        )
+      }
+    });
+    return () => {
+      cleanup;
+    };
+  }, [input]);
+  return (
+    <SafeArray>
+      <ScrollView>
+        <CustomListItem />
+      </ScrollView>
+    </SafeArray>
+  );
 };
 
 export default HomeScreen;
